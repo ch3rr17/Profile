68b46af72a59e8833c0a0abc962c0c33c769b516
ch3rr17 <cherr.batac@gmail.com>
2016-08-04 19:20:00 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -49,10 +49,10 @@ gulp.task('watch', function() {
 
 gulp.task('injectables', function() {
     var sources = gulp.src(paths, { read: false });
-    return gulp.src('index.html')
+    return gulp.src('./app/index.html')
         .pipe(wiredep())
-        .pipe(inject(sources))
-        .pipe(gulp.dest('.'));
+        .pipe(inject(sources), { relative: true })
+        .pipe(gulp.dest('./app'));
 });
 
 
