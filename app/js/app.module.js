4bf3e264fa1955f2303d7654bf467cc66e585fd1
ch3rr17 <cherr.batac@gmail.com>
2016-08-04 22:28:32 -0700

    modified app js

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -9,12 +9,12 @@
         	$urlRouterProvider.otherwise('main');
             
             //Create state for each page
-        	$stateProvider.state('main', { url: '/main', templateUrl: '/app/templates/main.html', controller: 'MainController as main'})
-        				  .state('projects', { url:'/projects', templateUrl: '/app/templates/projects.html', controller: 'ProjectController as project'})
-                          .state('projects.mortgagecalc', { url:'/mortcalc', templateUrl:'/app/templates/mortcalc.html', controller:'MortgageController as mort'})
-                          .state('projects.changecalc', {url:'/changecalc', templateUrl:'/app/templates/changecalc.html', controller:'ChangeController as change'})
-                          .state('projects.weather', {url:'/weather', templateUrl:'/app/templates/weather.html', controller:'WeatherController as weather'})
-                          .state('projects.todo', {url:'/todos', templateUrl:'/app/templates/todo.html', controller:'ToDoController as todo'});
+        	$stateProvider.state('main', { url: '/main', templateUrl: 'app/templates/main.html', controller: 'MainController as main'})
+        				  .state('projects', { url:'/projects', templateUrl: 'app/templates/projects.html', controller: 'ProjectController as project'})
+                          .state('projects.mortgagecalc', { url:'/mortcalc', templateUrl:'app/templates/mortcalc.html', controller:'MortgageController as mort'})
+                          .state('projects.changecalc', {url:'/changecalc', templateUrl:'app/templates/changecalc.html', controller:'ChangeController as change'})
+                          .state('projects.weather', {url:'/weather', templateUrl:'app/templates/weather.html', controller:'WeatherController as weather'})
+                          .state('projects.todo', {url:'/todos', templateUrl:'app/templates/todo.html', controller:'ToDoController as todo'});
         				  
         }]);
         
